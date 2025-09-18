0c323fc73eaccd8d5d4684742204c3127a29d96a
ManuL0129 <manumonla@hotmail.com>
2025-09-18 01:00:07 -0500

    holaaaa

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,29 +6,13 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  registrar(){
+    
   }
+  login(){
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
   }
+  validarCaptcha(){
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
   }
 }
