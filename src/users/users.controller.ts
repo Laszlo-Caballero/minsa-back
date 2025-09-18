0a48ac0123d9a00c8c2c76eecf00daaf8e31565c
laszlo-caballero <laszlocaballero@gmail.com>
2025-09-18 01:34:54 -0500

    feat: add TokenDto class for token validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { TokenDto } from './dto/token.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  registrar(){
-    
-  }
-  login(){
 
+  @Post('registrar')
+  registrar(@Body() userDto: CreateUserDto) {
+    return this.usersService.registrar(userDto);
   }
-  validarCaptcha(){
 
+  @Post('login')
+  login(@Body() userDto: CreateUserDto) {
+    return this.usersService.login(userDto);
+  }
+
+  @Post('validar-captcha')
+  validarCaptcha(@Body() tokenDto: TokenDto) {
+    return this.usersService.validarCaptcha(tokenDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { hash, compare } from 'bcryptjs';
+import axios from 'axios';
+import { TokenDto } from './dto/token.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
-  async registrar(userDto:CreateUserDto){
-    
-  }
-  login(){
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  async registrar(userDto: CreateUserDto) {
+    const hashPassword = await hash(userDto.password, 10);
 
-  }
-  validarCaptcha(){
+    const user = this.userRepository.create({
+      username: userDto.username,
+      password: hashPassword,
+    });
 
+    await this.userRepository.insert(user);
+
+    return {
+      message: 'Usuario registrado exitosamente',
+      data: null,
+    };
+  }
+  async login(userDto: CreateUserDto) {
+    const findUser = await this.userRepository.findOne({
+      where: { username: userDto.username },
+    });
+
+    if (!findUser) {
+      throw new HttpException('Error login', 400);
+    }
+
+    const isPasswordValid = await compare(userDto.password, findUser.password);
+
+    if (!isPasswordValid) {
+      throw new HttpException('Error login', 400);
+    }
+
+    return {
+      message: 'Login exitoso',
+      data: findUser,
+    };
+  }
+
+  async validarCaptcha(tokenDto: TokenDto) {
+    try {
+      const res = await axios.post(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET_KEY}&response=${tokenDto.token}`,
+      );
+      if (!res.data.success) {
+        throw new HttpException('Captcha verification failed', 400);
+      }
+
+      return {
+        status: 200,
+        message: 'Captcha verified successfully',
+        data: null,
+      };
+    } catch {
+      throw new HttpException('Captcha verification failed', 400);
+    }
   }
 }
